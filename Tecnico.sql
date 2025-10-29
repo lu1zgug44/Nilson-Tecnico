491e02885be3b4f0473c6368024dc2c6723a4e5a
LUIZ GUSTAVO CARVALHO DA SILVA COSTA <0000111775618xsp@al.educacao.sp.gov.br>
2025-10-29 14:31:00 -0300

    parte 3

--- Tecnico.sql
+++ Tecnico.sql
@@ -3,10 +3,29 @@ create database Tecnico;
 
 use Tecnico;
 
+create table tbcursos(
+codCurso INT primary key auto_increment,
+nomeCurso varchar(50),
+periodoCurso varchar(20)
+);
+
 create table tbprofessor(
 codProfessor INT primary key auto_increment,
 nomeProfessor varchar(50),
 emailProfessor varchar(50),
 fotoProfessor varchar(255),
 codStatus INT
+);
+
+create table tbformacao(
+codFormacao INT primary key auto_increment,
+nomeFormacao varchar(50),
+tipoFormacao varchar(30)
+);
+
+create table tblogin(
+codLogin INT primary key auto_increment,
+usuarioLogin varchar(50),
+senhaLogin varchar(10),
+Tipo INT
 );
