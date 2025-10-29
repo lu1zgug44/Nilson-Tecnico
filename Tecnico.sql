16b72dc7d0794503f6fa133be7600730daccacc2
LUIZ GUSTAVO CARVALHO DA SILVA COSTA <0000111775618xsp@al.educacao.sp.gov.br>
2025-10-29 16:24:05 -0300

    parte 9

--- Tecnico.sql
+++ Tecnico.sql
@@ -4,9 +4,14 @@ create database Tecnico;
 use Tecnico;
 
 create table tbstatus(
-codStatus INT primary key not null,
+codStatus INT primary key not null auto_increment,
 statusProfessor varchar(10)
 );
+
+insert into tbstatus (statusProfessor) 
+values('Ativo'),
+('Inativo'),
+('Afastado');
 
 create table tbprofessor(
 codProfessor INT primary key auto_increment,
@@ -17,11 +22,21 @@ codStatus INT not null,
 foreign key (codStatus) references tbStatus(codStatus)
 );
 
+insert into tbprofessor (nomeProfessor, emailProfessor, fotoProfessor, codStatus) 
+values('Marcos Paulo', 'marcos.paulo@escola.com', 'carlos.jpg', 1),
+('Eric Wallis', 'eric.wallis@escola.com', 'maria.jpg', 1),
+('Luiz Gustavo', 'luiz.gustavo@escola.com', 'joao.jpg', 2);
+
 create table tbformacao(
 codFormacao INT primary key auto_increment,
 nomeFormacao varchar(50),
 tipoFormacao varchar(30)
 );
+
+insert into tbformacao (nomeFormacao, tipoFormacao)
+values('Licenciatura em Matemática', 'Graduação'),
+('Engenharia de Software', 'Graduação'),
+('Mestrado em Educação', 'Pós-graduação');
 
 create table tbformacaoprofessor(
 codFormacaoProfessor INT primary key auto_increment,
@@ -32,11 +47,21 @@ foreign key (codFormacao) references tbformacao(codFormacao),
 foreign key (codProfessor) references tbprofessor(codProfessor)
 );
 
+insert into tbformacaoprofessor(codFormacao, codProfessor, instituicao)
+	values(1,1,'UNIP'),
+    (2,2,'USP'),
+    (3,3,'FATEC');
+
 create table tbcursos(
 codCurso INT primary key auto_increment,
 nomeCurso varchar(50),
 periodoCurso varchar(20)
 );
+
+insert into tbcursos(nomeCurso, periodoCurso)
+	values('Matemática','Noturno'),
+    ('Informática','Matutino'),
+    ('Pedagogia','Vespertino');
 
 create table tbcursoprofessor(
 codCursoProfessor INT primary key auto_increment,
@@ -46,10 +71,20 @@ foreign key (codCurso) references tbcursos(codCurso),
 foreign key (codProfessor) references tbprofessor(codProfessor)
 );
 
+insert into tbcursoprofessor(codCurso, codProfessor)
+	values(1,1),
+    (2,2),
+    (3,3);
+
 create table tbcoordenador(
 codCoordenador INT primary key auto_increment,
 nomeCoordenador varchar(50)
 );
+
+insert into tbcoordenador(nomeCoordenador)
+	values('Marcos Paulo'),
+    ('Eric Wallis'),
+    ('Luiz Gustavo');
 
 create table tbcursocoordenador(
 codCursoCoordenador INT primary key auto_increment,
@@ -58,6 +93,11 @@ codCoordenador INT not null,
 foreign key (codCurso) references tbcursos(codCurso),
 foreign key (codCoordenador) references tbcoordenador(codCoordenador)
 );
+
+insert into tbcursocoordenador(codCurso, codCoordenador)
+	values(1,1),
+    (2,2),
+    (3,3);
 
 create table tblogin(
 codLogin INT primary key auto_increment,
